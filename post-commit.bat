ce48e1ec469be63da781b466610d67247b484daf
elpatron68 <elpatron@cepheus.uberspace.de>
2015-11-20 21:19:33 +0100

    subversion post-commit (windows)

--- post-commit.bat
+++ post-commit.bat
@@ -2,11 +2,11 @@
 REM *************************************************************
 REM * Edit the following lines to suit your environment         *
 REM *************************************************************
-REM Set path to svnlook.exe
+REM Set full path to svnlook.exe
 SET SVNLOOK="C:\Program Files\VisualSVN Server\bin\svnlook.exe"
-REM Set path to python.exe
+REM Set full path to python.exe
 SET PYTHON="C:\Python27\python.exe"
-REM Set path to post.py
+REM Set full path to post.py
 SET POSTPY="C:\Svn2Mattermost\post.py"
 
 REM *************************************************************
@@ -21,6 +21,7 @@ REM *************************************************************
 setlocal EnableDelayedExpansion
 for /f "tokens=*" %%i in ('%SVNLOOK% log -r %TXN% %REPOS%') do set COMMENT=%%i
 for /f "tokens=*" %%i in ('%SVNLOOK% author -r %TXN% %REPOS%') do set AUTHOR=%%i
+
 REM *************************************************************
 REM * Hand it to post.py                                        *
 REM *************************************************************
